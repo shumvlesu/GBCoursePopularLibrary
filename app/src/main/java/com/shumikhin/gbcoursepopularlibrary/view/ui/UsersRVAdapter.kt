16913811e7d4ee3cfbc06352d212fbc41ed66386
shumikhin <sashumihin@gmail.com>
2021-09-20 16:17:41 +0300

    16 стр. 01:01:24

--- app/src/main/java/com/shumikhin/gbcoursepopularlibrary/view/ui/UsersRVAdapter.kt
+++ app/src/main/java/com/shumikhin/gbcoursepopularlibrary/view/ui/UsersRVAdapter.kt
@@ -16,31 +16,27 @@ import com.shumikhin.gbcoursepopularlibrary.view.UserItemView
 //nullable-значения функционального типа. Эта функция есть у любого значения функционального типа,
 //и её вызов вызывает саму функцию, которая и считается этим значением. Проще говоря,
 //presenter.itemClickListener?.invoke(holder) вызовет itemClickListener, если он не равен null.
-class UsersRVAdapter(val presenter: IUserListPresenter) : RecyclerView.Adapter<UsersRVAdapter.ViewHolder>() {
+class UsersRVAdapter(private val presenter: IUserListPresenter) : RecyclerView.Adapter<UsersRVAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
-        ItemUserBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent, false
-        )
+        ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
     ).apply {
         itemView.setOnClickListener { presenter.itemClickListener?.invoke(this) }
     }
 
     override fun getItemCount() = presenter.getCount()
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         presenter.bindView(holder.apply { pos = position })
+    }
 
     inner class ViewHolder(val vb: ItemUserBinding) : RecyclerView.ViewHolder(vb.root),
         UserItemView {
-
-        override var pos = -1
-
-        override fun setLogin(text: String) = with(vb) {
+            override var pos = -1
+            override fun setLogin(text: String) = with(vb) {
             tvLogin.text = text
-        }
+            }
 
-    }
+        }
 
 }
