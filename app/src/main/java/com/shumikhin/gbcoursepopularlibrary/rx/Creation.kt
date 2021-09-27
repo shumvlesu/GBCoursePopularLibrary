daec731bb91eda57f41eafdc7f0f9d954196a04a
shumikhin <sashumihin@gmail.com>
2021-09-27 17:25:07 +0300

    4 стр. 33:59

--- app/src/main/java/com/shumikhin/gbcoursepopularlibrary/rx/Creation.kt
+++ app/src/main/java/com/shumikhin/gbcoursepopularlibrary/rx/Creation.kt
@@ -1,6 +1,8 @@
 package com.shumikhin.gbcoursepopularlibrary.rx
 
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 
 class Creation {
 
@@ -19,8 +21,28 @@ class Creation {
     }
 
     class Consumer(val producer: Producer) {
+
+        val stringObserver = object : Observer<String> {
+            var disposable: Disposable? = null
+            override fun onComplete() {
+                println("onComplete")
+            }
+            override fun onSubscribe(d: Disposable?) {
+                disposable = d
+                println("onSubscribe")
+            }
+            override fun onNext(s: String?) {
+                println("onNext: $s")
+            }
+            override fun onError(e: Throwable?) {
+                println("onError: ${e?.message}")
+            }
+        }
+
+
         fun exec() {
         }
+
     }
 
 }
