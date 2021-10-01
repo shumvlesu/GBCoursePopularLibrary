1f642970aa15fab953bfabb5ac593a0ee90c04a5
shumikhin <sashumihin@gmail.com>
2021-10-01 16:05:37 +0300

    4го урок 30:26. Пример Single

--- app/src/main/java/com/shumikhin/gbcoursepopularlibrary/rx/Sources.kt
+++ app/src/main/java/com/shumikhin/gbcoursepopularlibrary/rx/Sources.kt
@@ -2,6 +2,7 @@ package com.shumikhin.gbcoursepopularlibrary.rx
 
 import android.util.Log
 import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
 import kotlin.random.Random
 
 class Sources {
@@ -19,7 +20,8 @@ class Sources {
             return listOf(true, false, true)[Random.nextInt(2)]
         }
 
-        //Completable подходит, когда получать значения не требуется, а нас интересует сам факт завершения
+        //Completable
+        // подходит, когда получать значения не требуется, а нас интересует сам факт завершения
         //какой-либо операции, например, запись в файл или базу данных. Для обработки этого источника
         //используется CompletableObserver, аналогичный Observer, но не имеющий onNext.
         //Короче говоря когда надо знать хорошо завершилась операция или завершилась ошибкой.
@@ -31,6 +33,33 @@ class Sources {
                 emitter.onError(RuntimeException("Пришёл false"))
             }
         }
+
+
+        //Single
+        //Источник аналогичен Observable, однако может выдать только одно значение. Single идеально
+        //подходит для HTTP-запросов, так как всегда ожидается только один ответ от сервера. Получение
+        //значения и завершение его работы — одно и то же событие. Вместо onNext и onComplete у его
+        //SingleObserver есть только один метод onSuccess, и он терминальный.
+        fun single() = Single.fromCallable {
+            //Для каждого вида источников есть свой аналог fromCallable. Например, в случае Completable
+            //используется fromAction, так как возвращаемое значение отсутствует. Мы используем его для
+            //создания Single с некоторым строковым значением. Нетрудно представить, например, как в теле
+            //fromCallable размещается код сетевого HTTP-запроса и возвращается его результат.
+            return@fromCallable "Привет!"
+        }
+
+        //Аналог создания Single
+        fun single2() = Single.create<Boolean> { emitter ->
+            val result = randomResultOperation()
+            if (result) {
+                emitter.onSuccess(result)
+            } else {
+                emitter.onError(RuntimeException("Пришёл false"))
+            }
+        }
+
+
+
 
 
     }
@@ -45,16 +74,26 @@ class Sources {
         /** Выполнение «потребления» */
         fun execute() {
 
-            //подписка на completable()
-            producer.completable().subscribe({
-                Log.d(TAG,"onComplete")
-            }, {
-                Log.e(TAG,"onError: ${it.message}")
-            })
+//            //подписка на completable()
+//            producer.completable().subscribe({
+//                Log.d(TAG,"onComplete")
+//            }, {
+//                Log.e(TAG,"onError: ${it.message}")
+//            })
+//            //Пример выполнения:
+//            //**** E/TAG: onError: Пришёл false
+
+
+            //подписка на single()
+            producer.single()
+                .map { it + it } //мапой предварительно сливаем
+                .subscribe({ s ->
+                    Log.d(TAG,"onSuccess: $s")
+                }, {
+                    Log.e(TAG,"onError: ${it.message}")
+                })
             //Пример выполнения:
-            //2021-10-01 15:48:22.841 16934-16934/com.shumikhin.gbcoursepopularlibrary E/TAG: onError: Пришёл false
-
-
+            // **** D/TAG: onSuccess: Привет!Привет!
 
 
 
