966fa7f1b30ed586ccf9ca85dd7794c741cce031
shumikhin <sashumihin@gmail.com>
2021-10-04 16:37:59 +0300

    4го урок 49:14. Пример Hot Observable, метода replay()

--- app/src/main/java/com/shumikhin/gbcoursepopularlibrary/rx/Sources.kt
+++ app/src/main/java/com/shumikhin/gbcoursepopularlibrary/rx/Sources.kt
@@ -3,7 +3,9 @@ package com.shumikhin.gbcoursepopularlibrary.rx
 import android.util.Log
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Maybe
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class Sources {
@@ -78,6 +80,31 @@ class Sources {
         }
 
 
+        //Горячий Observable
+        //отправляет данные независимо от того, подписан кто-нибудь на него или нет.
+        //Если никто его в это время не слушает, данные будут потеряны. Такой подход удобен, например, для
+        //оборачивания сокетных соединений, так как данные там идут непрерывно
+        fun hotObservable() = Observable
+            .interval(1, TimeUnit.SECONDS)
+            //Посредством оператора publish() формируется объект ConnectableObservable, метод которого — connect().
+            //Именно этот метод запускает работу Observable.
+            //При этом не имеет значения, есть подписчики или нет.
+            .publish() //один из способов  сделать обсервер горячим это вызвать метод publish()
+
+
+        //МЕТОДЫ ГОРЯЧЕГО ОБСЕРВЕРА//
+        //************У горячего Observable есть операторы, которые делают его работу отчасти сходной с работой холодного.****************//
+
+        //метод кэширует данные, и каждый новый подписчик получает полный набор
+        //данных, например, все сообщения чата, пришедшие до подписки, когда бы он ни подключился. Можно
+        //подумать, что таким образом мы получаем обычный холодный Observable, но это не так. Наш
+        //источник всё ещё горячий и его работа начнётся только после вызова метода connect()
+        fun hotObservableReplay() = Observable
+            .interval(1, TimeUnit.SECONDS)
+            //Метод replay() используется вместо publish()
+            //.publish()
+            .replay()
+
 
     }
 
@@ -117,14 +144,52 @@ class Sources {
             //Если случайный результат — true, то возвращаем onSuccess, в противном случае — onComplete.
             //Нетрудно представить, например, функцию получения текущего пользователя на месте
             //randomResultOperation.
-            producer.maybe()
-                .subscribe({ s ->
-                    Log.d(TAG,"onSuccess: $s")
-                }, {
-                    Log.e(TAG,"onError: ${it.message}")
-                }, {
-                    Log.d(TAG,"onComplete")
-                })
+//            producer.maybe()
+//                .subscribe({ s ->
+//                    Log.d(TAG,"onSuccess: $s")
+//                }, {
+//                    Log.e(TAG,"onError: ${it.message}")
+//                }, {
+//                    Log.d(TAG,"onComplete")
+//                })
+
+
+            //Подписка на hotObservable
+//            val hotObservable = producer.hotObservable()
+//            hotObservable.subscribe {Log.d(TAG, it.toString())} //Здесь еще не начинается вывод, мы просто подписались
+//            hotObservable.connect() //Начинаем вывод данных
+//            Thread.sleep(3000)
+//            hotObservable.subscribe {Log.d(TAG,"Отложенный подписчик: $it")}//Данные идут не с нуля, как у холодного обсервера,
+            //а именно как бы подключается к существующему потоку.
+            //Пример выполнения:
+            //2021-10-04 16:23:20.484 21366-21394/com.shumikhin.gbcoursepopularlibrary D/TAG: 0
+            //2021-10-04 16:23:21.493 21366-21394/com.shumikhin.gbcoursepopularlibrary D/TAG: 1
+            //2021-10-04 16:23:22.483 21366-21394/com.shumikhin.gbcoursepopularlibrary D/TAG: 2
+            //2021-10-04 16:23:22.483 21366-21394/com.shumikhin.gbcoursepopularlibrary D/TAG: Отложенный подписчик: 2
+            //2021-10-04 16:23:23.482 21366-21394/com.shumikhin.gbcoursepopularlibrary D/TAG: 3
+            //2021-10-04 16:23:23.482 21366-21394/com.shumikhin.gbcoursepopularlibrary D/TAG: Отложенный подписчик: 3
+            //2021-10-04 16:23:24.482 21366-21394/com.shumikhin.gbcoursepopularlibrary D/TAG: 4
+            //2021-10-04 16:23:24.482 21366-21394/com.shumikhin.gbcoursepopularlibrary D/TAG: Отложенный подписчик: 4
+
+            //МЕТОДЫ ГОРЯЧЕГО ОБСЕРВЕРА//
+            //************У горячего Observable есть операторы, которые делают его работу отчасти сходной с работой холодного.****************//
+
+            //Replay()
+            val hotObservableReplay = producer.hotObservableReplay()
+            hotObservableReplay.subscribe { Log.d(TAG, it.toString())} //Здесь еще не начинается вывод, мы просто подписались
+            hotObservableReplay.connect() //Начинаем вывод данных
+            Thread.sleep(3000)
+            hotObservableReplay.subscribe {Log.d(TAG,"Отложенный подписчик: $it")}//Данные идут не с нуля, как у холодного обсервера,
+            //Пример выполнения: Второй подписчик получает аналогично все данные хоть и подключился на 3 секунды позже
+            //2021-10-04 16:34:48.920 22135-22163/com.shumikhin.gbcoursepopularlibrary D/TAG: 0
+            //2021-10-04 16:34:49.920 22135-22163/com.shumikhin.gbcoursepopularlibrary D/TAG: 1
+            //2021-10-04 16:34:50.920 22135-22163/com.shumikhin.gbcoursepopularlibrary D/TAG: 2
+            //2021-10-04 16:34:50.920 22135-22135/com.shumikhin.gbcoursepopularlibrary D/TAG: Отложенный подписчик: 0
+            //2021-10-04 16:34:50.920 22135-22135/com.shumikhin.gbcoursepopularlibrary D/TAG: Отложенный подписчик: 1
+            //2021-10-04 16:34:50.920 22135-22135/com.shumikhin.gbcoursepopularlibrary D/TAG: Отложенный подписчик: 2
+            //2021-10-04 16:34:51.920 22135-22163/com.shumikhin.gbcoursepopularlibrary D/TAG: 3
+            //2021-10-04 16:34:51.920 22135-22163/com.shumikhin.gbcoursepopularlibrary D/TAG: Отложенный подписчик: 3
+
 
 
 
