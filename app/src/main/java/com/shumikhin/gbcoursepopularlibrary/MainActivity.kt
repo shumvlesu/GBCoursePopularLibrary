1f5c2a3e54bc849a60ac43bcd3044cc2a0e3fcc5
shumikhin <sashumihin@gmail.com>
2021-10-07 10:01:02 +0300

    Подготовка к дз.

--- app/src/main/java/com/shumikhin/gbcoursepopularlibrary/MainActivity.kt
+++ app/src/main/java/com/shumikhin/gbcoursepopularlibrary/MainActivity.kt
@@ -2,8 +2,9 @@ package com.shumikhin.gbcoursepopularlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import moxy.MvpAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : MvpAppCompatActivity(R.layout.activity_main), MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/shumikhin/gbcoursepopularlibrary/MainView.kt
@@ -0,0 +1,8 @@
+package com.shumikhin.gbcoursepopularlibrary
+
+import moxy.MvpView
+import moxy.viewstate.strategy.alias.AddToEndSingle
+
+
+@AddToEndSingle
+interface MainView : MvpView
