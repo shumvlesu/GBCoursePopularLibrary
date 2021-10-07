4cba5ddeebe46d0b779db771c8afb3f36d3d3b2b
shumikhin <sashumihin@gmail.com>
2021-10-07 09:54:05 +0300

    Подготовка к дз.

---
+++ app/src/main/java/com/shumikhin/gbcoursepopularlibrary/view/ConvertFragment.kt
@@ -0,0 +1,13 @@
+package com.shumikhin.gbcoursepopularlibrary.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.shumikhin.gbcoursepopularlibrary.R
+
+class ConvertFragment : Fragment() {
+
+
+}
