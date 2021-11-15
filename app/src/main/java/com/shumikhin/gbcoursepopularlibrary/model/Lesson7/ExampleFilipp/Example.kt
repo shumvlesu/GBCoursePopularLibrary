94ea31d6a2a4e93f986adb037285302818bdf601
shumikhin <sashumihin@gmail.com>
2021-11-15 16:45:01 +0300

    7 урок, пример Аннотации @Singletone (Scope). Время: 50:06

--- app/src/main/java/com/shumikhin/gbcoursepopularlibrary/model/Lesson7/ExampleFilipp/Example.kt
+++ app/src/main/java/com/shumikhin/gbcoursepopularlibrary/model/Lesson7/ExampleFilipp/Example.kt
@@ -4,34 +4,52 @@ import dagger.Component
 import dagger.Module
 import dagger.Provides
 import javax.inject.Inject
+import javax.inject.Singleton
 
 
 class Example {
 
     //вставь сюда зависимость
     @Inject
-    lateinit var a: String
+    //lateinit var a: String
+    lateinit var a: ExampleDependency
 
 }
 
+//1) Модуль который мы потом поместим в компонент даггера
 //Несколько зависимостей которые предоставляются через @Provides
 @Module
 class ExampleModule {
 
     //Предоставляю зависимость
     @Provides
+    @Singleton //Что бы ExampleDependency() не создавался каждый раз новый, указываем один из ключей scope - @Singleton
+
     //метод долен иметь тот же тип что и иньекция (@Inject, переменная - а)  что мы хотим получить.
     //Название не принципиально a() или exampleProvides()
-    fun exampleProvides(): String {
-        return "ABC"
+    fun exampleProvides(): ExampleDependency {
+        //return "ABC"
+        return ExampleDependency()
     }
+
+//    fun exampleProvides2(): String {
+//        return "CDE"
+//    }
 
 }
 
+//2)Компонент который мы заинжектим в класс Example в App
 //Применяется на интерфейс
 @Component(modules = [ExampleModule::class])
+@Singleton
 interface ExampleComponent {
     //прописываем методы которые прописывают зависимости
     //мы хотим прописать зависимость для класса Example, поэтому указываем в методе его
     fun inject(exampleInstance: Example)
+}
+
+
+//Это просто класс, который в место строковой переменной в примере ранее, мы теперь передаем в зависимости
+class ExampleDependency{
+    val exampleField = Math.random()
 }
