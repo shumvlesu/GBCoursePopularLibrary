879f4b91db556c6198749a777770c91bd717633c
shumikhin <sashumihin@gmail.com>
2021-11-22 16:50:43 +0300

    Урок 7. Практическая часть, 1:05:25, Делаю по методичке, а не по видео.

--- app/src/main/java/com/shumikhin/gbcoursepopularlibrary/di/modules/RepoModule.kt
+++ app/src/main/java/com/shumikhin/gbcoursepopularlibrary/di/modules/RepoModule.kt
@@ -2,6 +2,7 @@ package com.shumikhin.gbcoursepopularlibrary.di.modules
 
 import com.shumikhin.gbcoursepopularlibrary.model.RetrofitGitHubUsersRepo
 import com.shumikhin.gbcoursepopularlibrary.model.cache.IGithubUsersCache
+import com.shumikhin.gbcoursepopularlibrary.model.remote.GithubUsersRepo
 import com.shumikhin.gbcoursepopularlibrary.retrofit.IDataSource
 import com.shumikhin.gbcoursepopularlibrary.retrofit.IGitHubUsersRepo
 import com.shumikhin.gbcoursepopularlibrary.utils.INetworkStatus
@@ -19,5 +20,11 @@ class RepoModule {
         networkStatus: INetworkStatus,
         cache: IGithubUsersCache
     ): IGitHubUsersRepo = RetrofitGitHubUsersRepo(api, networkStatus, cache)
+
+//    @Singleton
+//    @Provides
+//    fun usersRepo(){
+//        return GithubUsersRepo()
+//    }
 
 }
